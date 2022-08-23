26f8893395f8af850ac888177f2b92ed045b3720
Serhii <yevstratovserhii@gmail.com>
2022-08-23 18:18:42 +0100

    Finish modal Log in

--- js/script.js
+++ js/script.js
@@ -3,18 +3,31 @@
 
 document.addEventListener('DOMContentLoaded', () =>{
 
-    const modalTrigger = document.querySelector('[data-modal]'),
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
       modal = document.querySelector('.modal'),
       modalCloseBtn = document.querySelector('[data-close]');
     
-    modalTrigger.addEventListener('click', () =>{
-        modal.classList.add('show');
-        modal.classList.remove('hide');
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () =>{
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    
     });
-
+ 
     modalCloseBtn.addEventListener('click', () =>{
         modal.classList.add('hide');
         modal.classList.remove('show');
+        document.body.style.overflow = '';
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+        }
     });
 }); 
 
