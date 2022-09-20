7ae52987345a085e1aa43c30b219869ebd0e5947
Serhii <yevstratovserhii@gmail.com>
2022-09-20 13:12:34 +0100

    fix Gellary page

--- js/script.js
+++ js/script.js
@@ -84,64 +84,66 @@ document.addEventListener('DOMContentLoaded', () => {
         toggle(dropdownContent, 'hide');
     });
 
-    class shopCard {
-        constructor (src, alt, title, descr, price, parentSelector, ...classes){
-            this.src = src;
-            this.alt = alt;
-            this.title = title;
-            this.descr = descr;
-            this.price = price;
-            this.parent = document.querySelector(parentSelector);
-            this.classes = classes;
-            this.transfer = 0.87;
-            this.changeToGBR();
-        }
+    
+    //------------------SHOP------------
+    // class shopCard {
+    //     constructor (src, alt, title, descr, price, parentSelector, ...classes){
+    //         this.src = src;
+    //         this.alt = alt;
+    //         this.title = title;
+    //         this.descr = descr;
+    //         this.price = price;
+    //         this.parent = document.querySelector(parentSelector);
+    //         this.classes = classes;
+    //         this.transfer = 0.87;
+    //         this.changeToGBR();
+    //     }
 
-        changeToGBR() {
-            this.price = (this.price * this.transfer).toFixed(0);
-        }
+    //     changeToGBR() {
+    //         this.price = (this.price * this.transfer).toFixed(0);
+    //     }
 
-        render() {
-            const element = document.createElement('div');
-            if (this.classes.length === 0) {
-                this.element = 'shop_item';
-                element.classList.add(this.element);
-            } else {
-                this.classes.forEach(className => element.classList.add(className));
-            }
+    //     render() {
+    //         const element = document.createElement('div');
+    //         if (this.classes.length === 0) {
+    //             this.element = 'shop_item';
+    //             element.classList.add(this.element);
+    //         } else {
+    //             this.classes.forEach(className => element.classList.add(className));
+    //         }
 
-            element.innerHTML = `
-                    <div class="img-shop"><img src = ${this.src} alt = ${this.alt}></div>
-                    <div class="fav-shop">
-                        <button type="submit" class="btn-fav-shop">
-                            <span class="icon-fav-shop"></span>
-                        </button>
-                    </div>
-                    <h3 class = "shop_title">${this.title}</h3>
-                    <div class = "shop_descr">${this.descr}</div>
-                    <div class="shop_price"><span>${this.price}</span> £</div>
-                </div>
-            `;
-            this.parent.append(element);
-        }
-    }
+    //         element.innerHTML = `
+    //                 <div class="img-shop"><img src = ${this.src} alt = ${this.alt}></div>
+    //                 <div class="fav-shop">
+    //                     <button type="submit" class="btn-fav-shop">
+    //                         <span class="icon-fav-shop"></span>
+    //                     </button>
+    //                 </div>
+    //                 <h3 class = "shop_title">${this.title}</h3>
+    //                 <div class = "shop_descr">${this.descr}</div>
+    //                 <div class="shop_price"><span>${this.price}</span> £</div>
+    //             </div>
+    //         `;
+    //         this.parent.append(element);
+    //     }
+    // }
 
-    const getResource = async (url) => {
-        const res = await fetch (url);
+    // const getResource = async (url) => {
+    //     const res = await fetch (url);
 
-        if (!res.ok) {
-            throw new Error (`Could not fetch ${url}, status: ${res.status}`);
-        }
+    //     if (!res.ok) {
+    //         throw new Error (`Could not fetch ${url}, status: ${res.status}`);
+    //     }
 
-        return await res.json();
-    };
+    //     return await res.json();
+    // };
 
-    getResource('http://localhost:3000/goods')
-        .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
-                new shopCard(img, altimg, title, descr, price, '.shop .container').render();
-            });
-        });
+    // getResource('http://localhost:3000/goods')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new shopCard(img, altimg, title, descr, price, '.shop .container').render();
+    //         });
+    //     });
 
 
     //_------- FORMS-----
@@ -240,6 +242,45 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:3000/goods')
         .then(data => data.json())
         .then (res => console.log(res));
+
+    
+    const eachSlide = document.querySelectorAll('.slides_gallery'),
+          prev = document.querySelector('.prev_arrow'),
+          next = document.querySelector('.next_arrow'),
+          slidesWrapper =document.querySelector('.slides_wrapper'),
+          allSlides = document.querySelector('.all_slides'),
+          width = window.getComputedStyle(slidesWrapper).width; 
+    let offset = 0;
+
+    allSlides.style.width = 100 * eachSlide.length + '%';
+    allSlides.style.display = 'flex';
+    allSlides.style.transition = '1s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    eachSlide.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    next.addEventListener('click', () => {
+        if(offset == +width.slice(0, width.length - 2) * (eachSlide.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+
+        allSlides.style.transform = `translateX(-${offset}px)`;
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (eachSlide.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        allSlides.style.transform = `translateX(-${offset}px)`;
+    });
 
 
 
